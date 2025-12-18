37661c218c7ee369a0ab1186caca8dd912d2913b
Alex <alex0982676657@gmail.com>
2025-12-18 10:04:29 +0800

    Modbud_TCP 重構

--- Modbus_TCP.cs
+++ Modbus_TCP.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+using static System.IO.Ports.ModBus_TCP;
 
 namespace System.IO.Ports
 {
@@ -12,114 +12,30 @@ namespace System.IO.Ports
     {
         private IConfiguration _config;
         public ILogger _logger;
-
-        public double ReadTimeout = 3000;
+        
         public ModBus_TCP(IConfiguration<ModBus_TCP> config, ILogger<ModBus_TCP> logger)
         {
             _config = config;
             _logger = logger;
         }
 
-        bool IsConnected(TcpClient client) => client?.Connected == true;
-        //{
-        //    if (client == null || client.Client == null) return false;
-        //    if (!client.Client.Connected) return false;
-        //    bool part1 = client.Client.Poll(0, SelectMode.SelectRead);
-        //    bool part2 = (client.Client.Available == 0);
-        //    if (part1 && part2) return false;
-        //    return true;
-        //}
+        public bool IsConnected(TcpClient client) => client?.Connected == true;
 
-        public (TcpClient tcp, CommandPacket packet) SendAndReceive(TcpClient tcp, string IP, int Port, byte[] send)
+        public (bool, ModBusTCPStream.CommandPacket) WriteAndRead(TcpClient _tcp, string _ip, int _port, short _address, short _quantity)
         {
-            if (send == null) throw new ArgumentNullException(nameof(send));
-            try
+            byte[] PDU = ModBusTCPStream.PDU(ModBus_TCP.FunctionCode.ReadHoldingRegisters, _address, _quantity);
+            using (ModBusTCPStream w1 = new ModBusTCPStream(PDU.Length/*PDU*/ + 1/*Unit*/))
             {
-                if (IsConnected(tcp) == false)
-                {
-                    DisconnectSafely(tcp);
-                    tcp = new TcpClient();
-                    tcp.Connect(IP, Port);
-                }
-
-                if (!tcp.Connected)
-                {
-                    _logger.LogError("Modbus_TCP 連線失敗");
-                    return (tcp, default);
-                }
-
-                var stream = tcp.GetStream();
-                tcp.ReceiveTimeout = (int)ReadTimeout;
-                tcp.SendTimeout = 5000;
-                //Write
-                stream.Write(send, 0, send.Length);
-
-                //Read 
-                byte[] tmp = new byte[1024];
-                int headerSize = Marshal.SizeOf<MbapHeader>();
-                MbapHeader? header = null;
-                var sw = Stopwatch.StartNew();
-                using (var s = new MemoryStream())
-                {
-                    while (sw.Elapsed.TotalMilliseconds <= ReadTimeout)
-                    {
-                        if (stream.DataAvailable)
-                        {
-                            var cnt = stream.Read(tmp, 0, tmp.Length);
-                            if (cnt == 0) break;
-
-                            s.Write(tmp, 0, cnt);
-                            var total_length = s.Length;
-
-                            if (header == null && total_length >= headerSize)
-                            {
-                                byte[] buf = s.GetBuffer();
-                                header = new MbapHeader
-                                {
-                                    TransactionId = (ushort)((buf[0] << 8) | buf[1]),
-                                    ProtocolId = (ushort)((buf[2] << 8) | buf[3]),
-                                    Length = (ushort)((buf[4] << 8) | buf[5]),
-                                    UnitId = buf[6]
-                                };
-
-                                var pdulength = header.Value.Length - 1;
-                                if (pdulength < 0)
-                                {
-                                    _logger.LogWarning("Modbus_TCP 負的 PDU 長度: {PDU}", pdulength);
-                                    break;
-                                }
-
-                                if (total_length >= headerSize + pdulength)
-                                {
-                                    var packet = new CommandPacket
-                                    {
-                                        Header = header.Value,
-                                        Data = new byte[pdulength]
-                                    };
-                                    Array.Copy(buf, headerSize, packet.Data, 0, packet.Data.Length);
-                                    return (tcp, packet);
-                                }
-                            }
-                        }
-                    }
-                }
+                w1.Write(PDU);
+                byte[] sendBuf = w1.ToArray();
+                var packet = w1.SendAndReceive(_tcp, _ip, _port, sendBuf, _logger);
+                if (packet == null || packet.Data == null || packet.Data.Length < 8)
+                    return (false, new ModBusTCPStream.CommandPacket());
+                return (true, packet);
             }
-            catch (Exception ex)
-            {
-                _logger.LogError("Modbus_TCP 連線異常");
-                DisconnectSafely(tcp);
-            }
-            return (tcp, default);
         }
 
-        public void DisconnectSafely(TcpClient tcp)
-        {
-            try { tcp?.Close(); tcp?.Dispose(); } catch { }
-        }
-
-
-
-        /// <summary> 同步範例 </summary>
+        /// 同步範例 
         //public void tcp_demo2()
         //{
         //    TcpClient tcp = new TcpClient();
@@ -141,6 +57,20 @@ namespace System.IO.Ports
         //    }
         //}
 
+        public enum FunctionCode
+        {
+            ReadHoldingRegisters = 0x03,
+            WriteSingleResister = 0x06,
+            WriteMultipleRegisters = 0x16,
+        }
+
+    }
+
+    /// <summary>
+    /// Modbus TCP 協定使用 Big Endian（高位在前），而 C# 預設是 Little Endian
+    /// </summary>
+    public class ModBusTCPStream : MemoryStream
+    {
         public class CommandPacket
         {
             public MbapHeader Header { get; set; }
@@ -156,21 +86,6 @@ namespace System.IO.Ports
             public byte UnitId;
         }
 
-        public enum FunctionCode
-        {
-
-            ReadHoldingRegisters = 0x03,
-            WriteSingleResister = 0x06,
-            WriteMultipleRegisters = 0x16,
-        }
-
-    }
-
-    /// <summary>
-    /// Modbus TCP 協定使用 Big Endian（高位在前），而 C# 預設是 Little Endian
-    /// </summary>
-    public class ModBusTCPStream : MemoryStream
-    {
         public static byte[] PDU(ModBus_TCP.FunctionCode FunctionCode, short Address, short Quentity)
         {
             using var pdu = new MemoryStream();
@@ -221,9 +136,86 @@ namespace System.IO.Ports
 
         }
 
+        public CommandPacket SendAndReceive(TcpClient tcp, string _ip, int _port, byte[] send, ILogger _logger, int _ReadTimeout = 3000)
+        {
+            if (send == null) throw new ArgumentNullException(nameof(send));
+            try
+            {
+                if (!tcp.Connected)
+                {
+                    _logger.LogError("Modbus_TCP 連線失敗");
+                    return default;
+                }
 
+                var stream = tcp.GetStream();
+                tcp.ReceiveTimeout = _ReadTimeout; // ReadTimeout 改為常數或傳入參數
+                tcp.SendTimeout = 5000;
+                //Write
+                stream.Write(send, 0, send.Length);
 
+                //Read 
+                byte[] tmp = new byte[1024];
+                int headerSize = Marshal.SizeOf<MbapHeader>();
+                MbapHeader? header = null;
+                var sw = Stopwatch.StartNew();
+                using (var s = new MemoryStream())
+                {
+                    while (sw.Elapsed.TotalMilliseconds <= _ReadTimeout)
+                    {
+                        if (stream.DataAvailable)
+                        {
+                            var cnt = stream.Read(tmp, 0, tmp.Length);
+                            if (cnt == 0) break;
 
+                            s.Write(tmp, 0, cnt);
+                            var total_length = s.Length;
+
+                            if (header == null && total_length >= headerSize)
+                            {
+                                byte[] buf = s.GetBuffer();
+                                header = new MbapHeader
+                                {
+                                    TransactionId = (ushort)((buf[0] << 8) | buf[1]),
+                                    ProtocolId = (ushort)((buf[2] << 8) | buf[3]),
+                                    Length = (ushort)((buf[4] << 8) | buf[5]),
+                                    UnitId = buf[6]
+                                };
+
+                                var pdulength = header.Value.Length - 1;
+                                if (pdulength < 0)
+                                {
+                                    _logger.LogWarning("Modbus_TCP 負的 PDU 長度: {PDU}", pdulength);
+                                    break;
+                                }
+
+                                if (total_length >= headerSize + pdulength)
+                                {
+                                    var packet = new CommandPacket
+                                    {
+                                        Header = header.Value,
+                                        Data = new byte[pdulength]
+                                    };
+                                    Array.Copy(buf, headerSize, packet.Data, 0, packet.Data.Length);
+                                    return packet;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception /*ex*/)
+            {
+                _logger.LogError("Modbus_TCP 連線異常");
+                try { tcp?.Close(); tcp?.Dispose(); } catch { }
+                tcp = null;
+            }
+            return default;
+        }
+        public static void DisconnectSafely(TcpClient tcp)
+        {
+            try { tcp?.Close(); tcp?.Dispose(); } catch { }
+            tcp = null;
+        }
 
     }
 
